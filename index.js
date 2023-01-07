e92e2d61ab8ae3adcb1d76c2bfc41799422be1d1
sanlokaja <sandyloka790@gmail.com>
2023-01-07 15:01:31 +0700

    docs: add endpoint products

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
 const mongooseConnect = require("./config/config.db");
-
+const fileUpload = require("./utils/fileUpload");
+// const path = require("path");
 const PORT = 2000;
 const app = express();
+
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+
+// ================================= Public File Access ================================= //
+
+// app.use("/public/files", express.static(path.join(__dirname, "/storages")));
+
+// ================================= End Public File Access ================================= //
+
+
+/* ================================= Import Middlewares ================================= */
+
+const middlewares = require("./middlewares/auth");
+
+/* ================================= End Import Middlewares ================================= */
 
 
 /* ================================= Import Controllers ================================= */
 
 const authController = require("./controllers/authController");
+const productController = require("./controllers/productController");
 
 /* ================================= End Import Controllers ================================= */
 
@@ -24,6 +42,14 @@ app.post('/api/v1/admin/register', authController.handleRegisterAdmin);
 app.post('/api/v1/admin/login', authController.handleLoginAdmin);
 
 /* ------------------------- End Admin ------------------------- */
+
+
+/* ------------------------- Products ------------------------- */
+
+app.get('/api/v1/products', productController.handleGetAllProducts);
+app.post('/api/v1/products', middlewares.authenticate, fileUpload.single("image"), productController.handleCreateProduct);
+
+/* ------------------------- End Products ------------------------- */
 
 /* ================================= End Define API ================================= */
 
