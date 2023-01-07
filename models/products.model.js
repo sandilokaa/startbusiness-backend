b464e51de62f5930d1d0f991d288cf8b5c13e9bf
sanlokaja <sandyloka790@gmail.com>
2023-01-07 14:46:02 +0700

    docs: add products model

---
+++ models/products.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const productsSchema = new Schema({
+	name: {
+		type: "String",
+		required: true,
+	},
+	price: {
+		type: "Number",
+		required: true,
+	},
+	category: {
+		type: "String",
+		required: true,
+	},
+	description: {
+		type: "String",
+		required: true,
+	},
+	picture: {
+		type: "String",
+		required: true,
+	},
+    adminId: {
+        type: mongoose.Schema.Types.ObjectId,
+		ref: "admins",
+    }
+});
+
+const products = mongoose.model("products", productsSchema);
+
+module.exports = products;
